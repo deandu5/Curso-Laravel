c13c5a76b9365b871e26a1cdedea458b9f23d5b0
deandu5 <agarciab12@miumg.edu.gt>
2021-04-19 15:20:05 -0600

    confirmar

--- database/migrations/2021_04_19_211009_crear_tabla_rol.php
+++ database/migrations/2021_04_19_211009_crear_tabla_rol.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CrearTablaRol extends Migration
 {
@@ -14,9 +14,11 @@ class CrearTablaRol extends Migration
     public function up()
     {
         Schema::create('rol', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('nombre', 50);
+            $table->bigIncrements('id');
+            $table->string('nombre', 50)->unique();
             $table->timestamps();
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_spanish_ci';
         });
     }
 
--- database/migrations/2021_04_19_211124_crear_tabla_permiso.php
+++ database/migrations/2021_04_19_211124_crear_tabla_permiso.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CrearTablaPermiso extends Migration
 {
@@ -14,10 +14,12 @@ class CrearTablaPermiso extends Migration
     public function up()
     {
         Schema::create('permiso', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('nombre', 50);
             $table->string('slug', 50);
             $table->timestamps();
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_spanish_ci';
         });
     }
 
