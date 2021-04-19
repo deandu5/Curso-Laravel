c38fd1f55c3f88b79e96a43c784feb8be485fb44
deandu5 <agarciab12@miumg.edu.gt>
2021-04-19 13:03:36 -0600

    commit

--- database/migrations/2021_04_19_174053_crear_tabla_permiso.php
+++ database/migrations/2021_04_19_174053_crear_tabla_permiso.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CrearTablaUsuario extends Migration
+class CrearTablaPermiso extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CrearTablaUsuario extends Migration
      */
     public function up()
     {
-        Schema::create('usuario', function (Blueprint $table) {
+        Schema::create('permiso', function (Blueprint $table) {
             $table->id('id');
-            $table->string('usuario', 50);
-            $table->string('password', 100);
             $table->string('Nombre', 50);
+            $table->string('Slug', 50);
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CrearTablaUsuario extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('usuario');
+        Schema::dropIfExists('permiso');
     }
 }
