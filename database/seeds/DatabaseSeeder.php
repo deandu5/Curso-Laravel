8b517947d5fa682c356cd0f5b8b7501cf8ad4647
deandu5 <agarciab12@miumg.edu.gt>
2021-04-19 19:11:41 -0600

    commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,30 +1,16 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Prophecy\Call\Call;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     *Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        $this->truncateTablas([
-            'rol',
-        ]);
-        $this->call(TablaRolSeeder::class);
+    //
     }
-
-    protected function truncateTablas(array $tablas)
-    {
-            DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
-        foreach($tablas as $tabla){
-            DB::table($tabla)->truncate();
-        }
-            DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
-    }
-}    
+}
